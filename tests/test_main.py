34dcbb1f8081619cbbae8925771fcb750db879bc
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-06 23:23:15 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,82 @@
-# write your code here
+"""
+In this test module, 'pytest-freezegun' was
+installed via pip to set datetime.date.today as
+'2022-02-02'
+"""
+
+
+import datetime
+import pytest
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "products,result",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ],
+            ["duck"]
+        ),
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 1, 9),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ],
+            ["salmon", "duck"]
+        ),
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 3),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 16),
+                    "price": 160
+                }
+            ],
+            []
+        )
+    ]
+)
+@pytest.mark.freeze_time('2022-02-02')
+def test_outdated_products(products, result):
+
+    assert outdated_products(products) == result
