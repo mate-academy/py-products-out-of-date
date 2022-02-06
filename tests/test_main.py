f9eb46f8968cc6475582910d73530675d4ca0d17
TJgkOsVP <sasha06052001@gmail.com>
2022-02-06 22:16:53 +0200

    'solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,13 @@
+import pytest
 import datetime
+from random import choice
 from unittest import mock
 from app.main import outdated_products
 
 
-@mock.patch("app.main.datetime")
-def test_all_products_fresh(mocked_datetime):
-    products = [
+@pytest.fixture()
+def products():
+    return [
         {
             "name": "salmon",
             "expiration_date": datetime.date(2022, 2, 13),
@@ -22,55 +24,30 @@ def test_all_products_fresh(mocked_datetime):
             "price": 160
         }
     ]
+
+
+@mock.patch("app.main.datetime")
+def test_all_products_fresh(mocked_datetime, products):
     mocked_datetime.date.today.return_value = \
         datetime.date(2022, 2, 13)
     assert outdated_products(products) == []
 
 
 @mock.patch("app.main.datetime")
-def test_all_products_outdated(mocked_datetime):
+def test_all_products_outdated(mocked_datetime, products):
     mocked_datetime.date.today.return_value = \
-        datetime.date(2022, 12, 9)
-    products = [
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 3),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 1, 4),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2021, 3, 5),
-            "price": 160
-        }
-    ]
-
+        datetime.date(2021, 12, 9)
+    for product in products:
+        product["expiration_date"] = \
+            datetime.date(2021, 12, choice((1, 5, 6, 2)))
     assert outdated_products(products) == ["salmon", "chicken", "duck"]
 
 
 @mock.patch("app.main.datetime")
-def test_not_all_products_outdated(mocked_datetime):
+def test_not_all_products_outdated(mocked_datetime, products):
     mocked_datetime.date.today.return_value = \
         datetime.date(2022, 2, 6)
-    products = [
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 3),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 3, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2021, 4, 23),
-            "price": 160
-        }
-    ]
-    assert outdated_products(products) == ["salmon", "duck"]
+    for product in products:
+        product["expiration_date"] = \
+            datetime.date(2022, 2, products.index(product) + 4)
+    assert outdated_products(products) == ["salmon", "chicken"]
