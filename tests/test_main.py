b29a4f2f54b6e0df006d65023aac6e442465a45b
Marko Khodan <khodanmarko@gmail.com>
2022-05-29 11:04:09 +0300

    solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+from freezegun import freeze_time
+from app.main import outdated_products
+import datetime
+
+@freeze_time("2022-02-02")
+class TestOutdatedProducts:
+    def test_should_return_only_outdated_products_name(self):
+        products = [
+            {
+                "name": "salmon",
+                "expiration_date": datetime.date(2022, 2, 10),
+                "price": 600
+            },
+            {
+                "name": "chicken",
+                "expiration_date": datetime.date(2022, 2, 5),
+                "price": 120
+            },
+            {
+                "name": "duck",
+                "expiration_date": datetime.date(2022, 2, 1),
+                "price": 160
+            }
+        ]
+        assert outdated_products(products) == ["duck"]
+
+
