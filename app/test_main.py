3cf214693fe6d8565e052390f182a8c8130e9103
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-06-04 20:19:38 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,47 @@
-# write your code here
+import datetime
+from freezegun import freeze_time
+
+import pytest
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "products,exception",
+    [
+        pytest.param(
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 6, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 6, 15),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 6, 3),
+                    "price": 160
+                },
+            ],
+            ["duck"],
+            id="Check normal work"),
+        pytest.param(
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date.today(),
+                    "price": 600
+                }
+            ],
+            [],
+            id="Check date == today",
+        )
+    ]
+)
+@freeze_time(f"{datetime.date.today()}")
+def test_outdated_product(products, exception):
+    assert outdated_products(products) == exception
