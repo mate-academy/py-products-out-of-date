1d670f2db149cd57a3e78389d60f56cec0dc4c6b
Serhii Leonenko <leonenko3149@gmail.com>
2022-06-05 19:08:15 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+import datetime
+
+from app.main import outdated_products
+
+
+class NewDate(datetime.date):
+    @classmethod
+    def today(cls):
+        return cls(2022, 2, 2)
+
+
+def test_outdated_products():
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 2),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]
+    datetime.date = NewDate
+
+    assert outdated_products(products) == ["duck"], (
+        f"Function should return ['duck'],"
+        f"when products equal {products}"
+    )
