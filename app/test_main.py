ebadfaac5ef7f34d1a655980f5045bb8d30e2fd5
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-06-07 13:36:11 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,64 @@
-# write your code here
+import datetime
+from unittest import mock
+
+import pytest
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "initial, expected",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ],
+            [
+                "duck"
+            ]
+        ),
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 3),
+                    "price": 160
+                }
+            ],
+            []
+        )
+    ]
+)
+@mock.patch("app.main.datetime")
+def test_return_outdated_products(
+        mocked_time,
+        initial,
+        expected
+):
+    mocked_time.date.today.return_value = datetime.date(2022, 2, 2)
+
+    assert outdated_products(initial) == expected
