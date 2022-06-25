cb6b1ed11e2bc4f5655daaab4c086876887cff71
SenchaPencha <SenchaPencha@mail.ru>
2022-06-25 22:01:07 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,31 +6,53 @@ from app.main import outdated_products
 
 
 @pytest.mark.parametrize(
-    "products, result",
+    "time, products, result",
     [
         (
-            [
-                {
-                    "name": "salmon",
-                    "expiration_date": datetime.date(2022, 2, 10),
-                    "price": 600
-                },
-                {
-                    "name": "chicken",
-                    "expiration_date": datetime.date(2022, 2, 5),
-                    "price": 120
-                },
-                {
-                    "name": "duck",
-                    "expiration_date": datetime.date(2022, 2, 1),
-                    "price": 160
-                }
-            ],
-            ['duck']
+                datetime.date(2022, 2, 2),
+                [
+                    {
+                        "name": "salmon",
+                        "expiration_date": datetime.date(2022, 2, 10),
+                        "price": 600
+                    },
+                    {
+                        "name": "chicken",
+                        "expiration_date": datetime.date(2022, 2, 5),
+                        "price": 120
+                    },
+                    {
+                        "name": "duck",
+                        "expiration_date": datetime.date(2022, 2, 1),
+                        "price": 160
+                    }
+                ],
+                ['duck']
+        ),
+        (
+                datetime.date(2024, 2, 2),
+                [
+                    {
+                        "name": "salmon",
+                        "expiration_date": datetime.date(2022, 2, 10),
+                        "price": 600
+                    },
+                    {
+                        "name": "chicken",
+                        "expiration_date": datetime.date(2022, 2, 5),
+                        "price": 120
+                    },
+                    {
+                        "name": "duck",
+                        "expiration_date": datetime.date(2022, 2, 1),
+                        "price": 160
+                    }
+                ],
+                ["salmon", "chicken", 'duck']
         )
     ]
 )
 @mock.patch("app.main.datetime")
-def test_outdated_products(mocked_datetime, products, result):
-    mocked_datetime.date.today.return_value = datetime.date(2022, 2, 2)
+def test_outdated_products(mocked_datetime, time, products, result):
+    mocked_datetime.date.today.return_value = time
     assert outdated_products(products) == result
