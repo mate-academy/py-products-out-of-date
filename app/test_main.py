029c48c6cff65e3f001f73db1d9860a9c096261a
Andriy Demeshko <ande777complaint@gmail.com>
2022-07-28 15:04:28 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+from unittest import mock
+
+import datetime
+
+from app.main import outdated_products
+
+
+products = [{"name": "salmon", "expiration_date": datetime.date(2022, 7, 9), "price": 600}]
+
+
+@mock.patch("app.main.datetime")
+def test_out_date(mocked_date):
+    mocked_date.date.today.return_value = datetime.date(2022, 7, 10)
+    assert outdated_products(products) == ["salmon"]
+
+
+@mock.patch("app.main.datetime")
+def test_in_date(mocked_date):
+    mocked_date.date.today.return_value = datetime.date(2022, 7, 9)
+    assert outdated_products(products) == []
