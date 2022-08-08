cc62c56b6de3c881cc1b0c739a97f2b1ada39baa
Andrei Korotenko <korotenko.qndroid@gmail.com>
2022-08-08 14:49:04 +0300

    update progress

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import datetime
 
 
+# update progress
 def outdated_products(products: list):
     return [product["name"] for product in products
             if product["expiration_date"] < datetime.date.today()]
