41b4503238ab9606bf5641182b340c7f5d766869
Inna Kushnir <innakushnir63@gmail.com>
2022-08-13 22:55:32 +0300

    Solutions

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,27 @@
 from app.main import outdated_products
-import pytest
+from unittest import mock
 import datetime
+import pytest
 
 
 @pytest.fixture
 def list_products():
     return ([
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 8, 6),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 8, 19),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 9, 2),
-            "price": 160}
-    ])
+         {
+             "name": "salmon",
+             "expiration_date": datetime.date(2022, 8, 6),
+             "price": 600
+         },
+         {
+             "name": "chicken",
+             "expiration_date": datetime.date(2022, 8, 19),
+             "price": 120
+         },
+         {
+             "name": "duck",
+             "expiration_date": datetime.date(2022, 9, 2),
+             "price": 160}
+     ])
 
 
 def test_expiration_all_dates_less_date(list_products):
@@ -28,6 +29,7 @@ def test_expiration_all_dates_less_date(list_products):
     list_products[1]["expiration_date"] = datetime.date(2022, 7, 15)
     list_products[2]["expiration_date"] = datetime.date(2022, 6, 1)
     assert outdated_products(list_products) == ["salmon", "chicken", "duck"]
+
 
 
 def test_expiration_all_dates_norm_date(list_products):
