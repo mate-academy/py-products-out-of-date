8c57e97ee0003c96fbc12853c6947dd71ec0eeae
Inna Kushnir <innakushnir63@gmail.com>
2022-08-14 08:09:18 +0300

    J

--- app/test_main.py
+++ app/test_main.py
@@ -13,12 +13,12 @@ def list_products():
         "name": "chicken",
         "expiration_date": datetime.date(2022, 8, 19),
         "price": 120
-     },
+      },
         {
         "name": "duck",
         "expiration_date": datetime.date(2022, 9, 2),
         "price": 160
-     }
+      }
     ])
 
 
