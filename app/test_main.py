d2831723e37522c1557b76812eacf316d70c20a1
Inna Kushnir <innakushnir63@gmail.com>
2022-08-14 07:36:10 +0300

    Sol

--- app/test_main.py
+++ app/test_main.py
@@ -8,13 +8,10 @@ def list_products():
     return ([{
         "name": "salmon",
         "expiration_date": datetime.date(2022, 8, 6),
-        "price": 600
-    },
-       {
-        "name": "chicken",
+        "price": 600},
+       {"name": "chicken",
         "expiration_date": datetime.date(2022, 8, 19),
-        "price": 120
-        },
+        "price": 120},
        {"name": "duck",
         "expiration_date": datetime.date(2022, 9, 2),
         "price": 160}])
