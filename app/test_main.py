afef2f50f52e86ff9eaab3b4d4ce271188a733bb
Inna Kushnir <innakushnir63@gmail.com>
2022-08-14 07:40:16 +0300

    Pythons

--- app/test_main.py
+++ app/test_main.py
@@ -9,12 +9,14 @@ def list_products():
         "name": "salmon",
         "expiration_date": datetime.date(2022, 8, 6),
         "price": 600},
-        {"name": "chicken",
-         "expiration_date": datetime.date(2022, 8, 19),
-         "price": 120},
-        {"name": "duck",
-         "expiration_date": datetime.date(2022, 9, 2),
-         "price": 160}])
+        {
+          "name": "chicken",
+          "expiration_date": datetime.date(2022, 8, 19),
+          "price": 120},
+        {
+          "name": "duck",
+          "expiration_date": datetime.date(2022, 9, 2),
+          "price": 160}])
 
 
 def test_expiration_all_dates_less_date(list_products):
