f3436df62c9934e84c0e9ef2f6d0461d0f559000
Inna Kushnir <innakushnir63@gmail.com>
2022-08-16 11:10:51 +0300

    Sol

--- app/test_main.py
+++ app/test_main.py
@@ -7,20 +7,20 @@ import pytest
 def list_products():
     return (
         [
-          {
+         {
             "name": "salmon",
             "expiration_date": datetime.date(2022, 8, 6),
             "price": 600
           },
-          {
+         {
             "name": "chicken",
             "expiration_date": datetime.date(2022, 8, 19),
             "price": 120
           },
-          {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 9, 2),
-            "price": 160
+         {
+             "name": "duck",
+             "expiration_date": datetime.date(2022, 9, 2),
+             "price": 160
           }
         ]
     )
