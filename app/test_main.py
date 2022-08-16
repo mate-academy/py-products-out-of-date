75d295db385b02c40e6f0f57c450a9a6700e1df2
Inna Kushnir <innakushnir63@gmail.com>
2022-08-16 14:07:37 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,9 @@
 from app.main import outdated_products
 import datetime
-import pytest
 
 
-@pytest.fixture
-def list_products():
-    return ([
+def test_expiration_all_dates_less_date():
+    list_products = [
         {
             "name": "salmon",
             "expiration_date": datetime.date(2022, 8, 6),
@@ -13,36 +11,56 @@ def list_products():
         },
         {
             "name": "chicken",
-            "expiration_date": datetime.date(2022, 8, 19),
+            "expiration_date": datetime.date(2022, 8, 9),
             "price": 120
         },
         {
             "name": "duck",
-            "expiration_date": datetime.date(2022, 9, 2),
+            "expiration_date": datetime.date(2022, 8, 2),
             "price": 160
         }
-          ]
-           )
-
-
-def test_expiration_all_dates_less_date(list_products):
-    # change date
-    list_products[0]["expiration_date"] = datetime.date(2022, 7, 31)
-    list_products[1]["expiration_date"] = datetime.date(2022, 7, 15)
-    list_products[2]["expiration_date"] = datetime.date(2022, 6, 1)
+    ]
     assert outdated_products(list_products) == ["salmon", "chicken", "duck"]
 
 
-def test_expiration_all_dates_norm_date(list_products):
-    # change date
-    list_products[0]["expiration_date"] = datetime.date(2022, 8, 24)
-    list_products[1]["expiration_date"] = datetime.date(2022, 8, 19)
-    list_products[2]["expiration_date"] = datetime.date(2022, 8, 30)
+def test_expiration_all_dates_norm_date():
+    list_products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 8, 31),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 9, 9),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 8, 22),
+            "price": 160
+        }
+    ]
+
     assert outdated_products(list_products) == []
 
 
-def test_expiration_dates_are_different_date(list_products):
-    list_products[0]["expiration_date"] = datetime.date(2022, 7, 12)
-    list_products[1]["expiration_date"] = datetime.date(2022, 8, 19)
-    list_products[2]["expiration_date"] = datetime.date(2022, 9, 30)
-    assert outdated_products(list_products) == ["salmon"]
+def test_expiration_dates_are_different_date():
+    list_products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 8, 6),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 8, 29),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 8, 2),
+            "price": 160
+        }
+    ]
+    assert outdated_products(list_products) == ["salmon", "duck"]
