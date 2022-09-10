0c870af9569282a3d31b9da4a0b23d7541554946
Paul Starodub <amorallex@gmail.com>
2022-09-10 21:06:45 +0300

    solo

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,55 @@
-# write your code here
+import pytest
+
+import datetime
+
+from unittest import mock
+
+from app.main import outdated_products
+
+
+@pytest.fixture()
+def mocked_datetime():
+    with mock.patch("datetime.date") as mocked_datetime_date:
+        mocked_datetime_date.return_value = datetime.date.today()
+
+
+def test_out_fated_product(mocked_datetime):
+    el = ([
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 9, 9),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ])
+    assert outdated_products(el) == ['salmon', 'chicken', 'duck']
+
+
+def test_out_fated2_product(mocked_datetime):
+    el = ([
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 9, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ])
+    assert outdated_products(el) == ['chicken', 'duck']
