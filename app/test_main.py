375a7cd9b7ff18579eca4142f2e53f4514fc926c
Vlad Magdenko <vladearth7@gmail.com>
2022-09-20 18:49:29 +0300

    'Solution4'

--- app/test_main.py
+++ app/test_main.py
@@ -8,37 +8,37 @@ from app.main import outdated_products
     "dict_in, result, date",
     [
         ([
-             {
-                 "name": "salmon",
-                 "expiration_date": datetime.date(2022, 2, 10),
-                 "price": 600
-             },
-             {
-                 "name": "chicken",
-                 "expiration_date": datetime.date(2022, 2, 5),
-                 "price": 120
-             },
-             {
-                 "name": "duck",
-                 "expiration_date": datetime.date(2022, 2, 1),
-                 "price": 160
-             }], ["duck"], datetime.date(2022, 2, 2)),
+            {
+                "name": "salmon",
+                "expiration_date": datetime.date(2022, 2, 10),
+                "price": 600
+            },
+            {
+                "name": "chicken",
+                "expiration_date": datetime.date(2022, 2, 5),
+                "price": 120
+            },
+            {
+                "name": "duck",
+                "expiration_date": datetime.date(2022, 2, 1),
+                "price": 160
+            }], ["duck"], datetime.date(2022, 2, 2)),
         ([
-             {
-                 "name": "salmon",
-                 "expiration_date": datetime.date(2022, 2, 10),
-                 "price": 600
-             },
-             {
-                 "name": "chicken",
-                 "expiration_date": datetime.date(2022, 2, 5),
-                 "price": 120
-             },
-             {
-                 "name": "duck",
-                 "expiration_date": datetime.date(2022, 2, 1),
-                 "price": 160
-             }], [], datetime.date(2022, 1, 29)),
+            {
+                "name": "salmon",
+                "expiration_date": datetime.date(2022, 2, 10),
+                "price": 600
+            },
+            {
+                "name": "chicken",
+                "expiration_date": datetime.date(2022, 2, 5),
+                "price": 120
+            },
+            {
+                "name": "duck",
+                "expiration_date": datetime.date(2022, 2, 1),
+                "price": 160
+            }], [], datetime.date(2022, 1, 29)),
     ]
 )
 @patch("app.main.datetime")
