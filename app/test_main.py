9fc27131b77a7d3e0000c0a9b3d1dc6db3d80a1c
Davyd Kryvko <davidkrivko@gmail.com>
2022-09-27 19:31:23 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+import datetime
+
+import pytest
+
+from app.main import outdated_products
+
+
+@pytest.fixture()
+def products():
+    return [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 9, 27),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 9, 26),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }]
+
+
+def test_should_return_right_list(products):
+    assert outdated_products(products) == ["chicken", "duck"]
