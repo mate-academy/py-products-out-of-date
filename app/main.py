398057fb4518dc82bad3c08fbbb1cfc2320ad4ef
Oleksandr Panasiuk <provizor.307@gmail.com>
2022-10-03 13:09:30 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -4,3 +4,23 @@ import datetime
 def outdated_products(products: list):
     return [product["name"] for product in products
             if product["expiration_date"] < datetime.date.today()]
+
+
+if __name__ == '__main__':
+    print(outdated_products([
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]))
