2c3c349967c2448ebb091137dbc0617a2a7b0677
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-10-04 18:01:59 +0300

    Solotion_2

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,28 @@
 import datetime
-import pytest
 from unittest import mock
 
 from app.main import outdated_products
 
-
-@pytest.fixture()
-def list_of_outdated_products():
-    products = ([
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 2, 1),
-            "price": 160
-        }
-    ])
-    return products
+product = ([
+    {
+        "name": "salmon",
+        "expiration_date": datetime.date(2022, 2, 10),
+        "price": 600
+    },
+    {
+        "name": "chicken",
+        "expiration_date": datetime.date(2022, 2, 5),
+        "price": 120
+    },
+    {
+        "name": "duck",
+        "expiration_date": datetime.date(2022, 2, 1),
+        "price": 160
+    }
+])
 
 
 @mock.patch("app.main.datetime")
 def test_outdated_products(mocked_datetime):
-    mocked_datetime.date.today.return_value = datetime.date(2022, 2, )
-    assert outdated_products(list_of_outdated_products) == ["duck"]
+    mocked_datetime.date.today.return_value = datetime.date(2022, 2, 5)
+    assert outdated_products(product) == ["duck"]
