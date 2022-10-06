39da265dde3848594b42b0db8014985f9a3f0ef8
Vitalii Shevchuk <shevch.nl@gmail.com>
2022-10-07 00:19:19 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+import datetime
+import pytest
+from unittest import mock
+from app.main import outdated_products
+
+
+@pytest.fixture
+def template_list():
+    products =[
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]
+    yield products
+
+
+@mock.patch("app.main.datetime")
+def test_outdated_products(mocked_date, template_list):
+    mocked_date.date.today.return_value = datetime.date(2022, 2, 6)
+    assert outdated_products(template_list) == ["chicken", "duck"]
