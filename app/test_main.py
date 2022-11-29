ec84142510f20066e1cb745ea553c42ba73dd70e
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-11-29 23:55:37 +0200

    Cheet

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from unittest import mock
+import datetime
+from app.main import outdated_products
+
+
+
+def test_with_duck_out_date():
+    list_goods = [
+    {
+        "name": "salmon",
+        "expiration_date": datetime.date(2022, 11, 29),
+        "price": 600
+    },
+    {
+        "name": "chicken",
+        "expiration_date": datetime.date(2022, 11, 30),
+        "price": 120
+    },
+    {
+        "name": "duck",
+        "expiration_date": datetime.date(2022, 11, 28),
+        "price": 160
+    }
+]
+    assert outdated_products(list_goods) == ["duck"]
