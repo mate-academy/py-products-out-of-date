fd0cef5eca945fe0a1a021205e248117820d6273
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-30 16:51:36 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import datetime
+from app.main import outdated_products
+
+
+def test_date_of_goods() -> None:
+    assert outdated_products([{
+        "name": "salmon",
+        "expiration_date": datetime.date(2025, 2, 10),
+        "price": 600
+    },
+        {
+        "name": "salm",
+        "expiration_date": datetime.date(2022, 12, 29),
+        "price": 600
+    },
+        {
+        "name": "salo",
+        "expiration_date": datetime.date(2022, 12, 30),
+        "price": 600
+    }
+    ]) == ["salm"]
