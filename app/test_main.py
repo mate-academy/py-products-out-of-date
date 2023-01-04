4bfc8ee732313394a6d9b30e117de87fc888ad5f
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-01-04 17:31:08 +0200

    Products of date

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from app.main import outdated_products
+import datetime
+
+
+def test_should_check_today() -> None:
+    assert outdated_products(
+        [
+            {
+                "name": "salmon",
+                "expiration_date": datetime.date(2023, 1, 4),
+                "price": 600
+            }
+        ]
+    ) == []
+
+
+def test_should_check_yesterday() -> None:
+    assert outdated_products(
+        [
+            {
+                "name": "salmon",
+                "expiration_date": datetime.date(2023, 1, 3),
+                "price": 600
+            }
+        ]
+    ) == ["salmon"]
