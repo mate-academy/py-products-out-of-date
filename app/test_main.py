985561bd3a379ea9a2374af5f8c324351c7c0319
Vitalii Panochko <vpapplic@gmail.com>
2023-01-05 17:23:34 +0200

    redone tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+from app.main import outdated_products
+import datetime
+from unittest import mock
+
+products = [
+    {
+        "name": "salmon",
+        "expiration_date": datetime.date.today(),
+        "price": 600
+    },
+    {
+        "name": "chicken",
+        "expiration_date": datetime.date(2022, 2, 5),
+        "price": 120
+    },
+    {
+        "name": "duck",
+        "expiration_date": datetime.date(2022, 2, 1),
+        "price": 160
+    }
+]
+
+
+def test_today() -> None:
+    current_day = datetime.date.today()
+    with mock.patch("datetime.date") as mocked_today:
+
+        mocked_today.today.return_value = current_day
+        assert outdated_products(products) == ["chicken", "duck"]
+
+
+def test_datetime_func() -> None:
+    need_day = datetime.date(2022, 2, 2)
+    with mock.patch("datetime.date") as mocked_func:
+        mocked_func.today.return_value = need_day
+        assert outdated_products(products) == ["duck"]
