e6457749d3e08529dab9f58d275299c6e3861ece
Oleksandr Tsvirko <oleksandr.tsvirko@gmail.com>
2023-01-07 21:42:48 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+import datetime
+from unittest import mock
+from app.main import outdated_products
+from typing import Any
+
+
+# @pytest.fixture()
+# def mocked_datetime():
+#     with mock.patch("app.main.datetime") as mocked_date:
+#         return mocked_date
+
+
+@mock.patch("app.main.datetime")
+def test_outdated_products(mocked_datetime: Any) -> None:
+    mocked_datetime.date.today.return_value = datetime.date(2022, 2, 5)
+    assert outdated_products([
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]) == ["duck"]
