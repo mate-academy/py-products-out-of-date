b7c67697218d4a985c24fab3eeec6628e8ecfd94
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-02-01 22:12:48 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,3 +4,11 @@ import datetime
 def outdated_products(products: list) -> list:
     return [product["name"] for product in products
             if product["expiration_date"] < datetime.date.today()]
+
+
+user = [{
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        }]
+print(datetime.date.today())
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+import pytest
+from typing import Callable
+from unittest import mock
+import datetime
+from app.main import outdated_products
+
+
+@pytest.fixture()
+def mocked_datetime() -> None:
+    with mock.patch("app.main.datetime") as mock_datetime:
+        yield mock_datetime
+
+
+@pytest.mark.parametrize(
+    "product, result",
+    [
+        ([{
+            "name": "salmon",
+            "expiration_date": datetime.date.today(),
+            "price": 600
+        }], []),
+        ([{
+            "name": "salmon",
+            "expiration_date": datetime.date(2023, 1, 31),
+            "price": 600
+        }], ["salmon"]),
+    ]
+)
+def test_cryptocurrency_action_result(
+        mocked_datetime: Callable,
+        product: dict,
+        result: bool
+) -> None:
+    mocked_datetime.date.today.return_value = datetime.date.today()
+    assert outdated_products(product) == result
