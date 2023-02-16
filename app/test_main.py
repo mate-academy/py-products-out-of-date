b0df97082788bf0b26a5844d7e8933bd73cd0fdc
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-16 12:50:30 +0200

    1 commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+import pytest
+import datetime
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "products, result", [
+        ([{
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        }], ["salmon"]),
+        ([{
+            "name": "salmon",
+            "expiration_date": datetime.date(2024, 2, 10),
+            "price": 600
+        }], []),
+        ([{
+            "name": "salmon",
+            "expiration_date": datetime.date(2023, 2, 16),
+            "price": 600
+        }], []),
+        ([{
+            "name": "salmon",
+            "expiration_date": datetime.date(2023, 2, 15),
+            "price": 600
+        }], ["salmon"]),
+
+    ])
+def test_outdated_products(products: list[dict], result: list[str]) -> None:
+    assert outdated_products(products) == result
