0bf6504b60494ffadb8835ba9d5d0af47dcea7b8
Andy <andy77andy@ukr.net>
2023-03-05 21:17:30 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
+import datetime
+import pytest
+from unittest import mock
+
+from app.main import outdated_products
+
+
+@pytest.fixture()
+def products_template() -> list:
+    return [{"name": "cheese",
+             "expiration_date": datetime.date(2022, 2, 5),
+             "price": 600}]
+
+
+@mock.patch("app.main.datetime")
+def test_prod_exist(mocked_datetime: mock, products_template: list) -> None:
+    mocked_datetime.date.today.return_value = datetime.date(2022, 1, 10)
+    assert outdated_products(products_template) == []
+
+
+@mock.patch("app.main.datetime")
+def test_out_products(mocked_datetime: mock, products_template: list) -> None:
+    mocked_datetime.date.today.return_value = datetime.date(2022, 2, 10)
+    assert outdated_products(products_template) == ["cheese"]
 # write your code here
