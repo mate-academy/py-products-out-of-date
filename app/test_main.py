452f80fba3a99d008a7e4514e925c7f8a6318539
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-03-04 19:10:05 -0600

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,47 @@
-# write your code here
+import datetime
+
+import pytest
+
+from typing import Callable
+from unittest import mock
+
+from app.main import outdated_products
+
+
+dict_products = [
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ]
+
+@pytest.fixture()
+def mocked_func() -> None:
+    with mock.patch("datetime.date.today") as mocked_today:
+        yield mocked_today
+
+
+@pytest.mark.parametrize(
+    "cur_date,result",
+    [
+        (datetime.date(2022, 2, 5), ["duck"]),
+        (datetime.date(2022, 2, 2), ["duck"]),
+        (datetime.date(2021, 2, 3), []),
+    ]
+)
+def test_outdated_products(cur_date: datetime.date, result: list):
+    class NewDate(datetime.date):
+        @classmethod
+        def today(cls):
+            return cur_date
+
+    datetime.date = NewDate
+
+    assert outdated_products(dict_products) == result
+
