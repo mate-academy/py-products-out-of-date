7c7ea48b67ece0447347c0932bfe020183b557c0
WalDragnir <akgk530@gmail.com>
2023-03-23 19:58:55 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,54 @@
-# write your code here
+from unittest import mock
+from datetime import date
+from unittest.mock import MagicMock
+import pytest
+from typing import Callable
+from app.main import outdated_products
+
+
+@pytest.fixture()
+def mocked_datetime() -> Callable:
+    with mock.patch("datetime.date") as mocked_time:
+        yield mocked_time
+
+
+def test_today_products_must_not_expired(mocked_datetime: MagicMock) -> None:
+    mocked_datetime.today.return_value = date(2022, 2, 2)
+    assert outdated_products([
+        {
+            "name": "salmon",
+            "expiration_date": date(2022, 3, 23),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": date(2022, 2, 2),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": date(2022, 1, 20),
+            "price": 160
+        }
+    ]) == ["duck"]
+
+
+def test_expiration_day_yesterday_outdated(mocked_datetime: MagicMock) -> None:
+    mocked_datetime.today.return_value = date(2022, 2, 2)
+    assert outdated_products([
+        {
+            "name": "salmon",
+            "expiration_date": date(2022, 3, 23),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": date(2022, 2, 1),
+            "price": 160
+        }
+    ]) == ["duck"]
