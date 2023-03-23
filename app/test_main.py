81eaa89ae51f059fc571f40b9fceb03d0555f476
Andrii Kozlovskyi <0503424511@ukr.net>
2023-03-23 10:30:23 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+import pytest
+import datetime
+from app.main import outdated_products
+
+
+@pytest.fixture()
+def products() -> list:
+    mocked_today = datetime.date.today()
+    return (
+        {
+            "name": "salmon",
+            "expiration_date": mocked_today,
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": mocked_today - datetime.timedelta(days=1),
+            "price": 120
+        },
+    )
+
+
+def test_today_products(products: list) -> None:
+    assert "salmon" not in outdated_products(products)
+
+
+def test_yesterday_product(products: list) -> None:
+    assert "chicken" in outdated_products(products)
