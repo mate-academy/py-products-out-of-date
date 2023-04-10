c7b6c169d2c0222335fd32e4a18ae09dc73abf60
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-04-10 17:19:16 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,42 +1,32 @@
-import pytest
 import datetime
+import pytest
 from unittest import mock
 from app.main import outdated_products
 
 
 @pytest.fixture()
-def mock_today() -> None:
-    with mock.patch("datetime.datetime") as mocked_function:
-        yield mocked_function
+def mock_time() -> None:
+    with mock.patch("app.main.datetime") as mocked_time:
+        yield mocked_time
 
 
-@pytest.mark.parametrize(
-    "products,result",
-    [
-        (
-            [
-                {
-                    "name": "salmon",
-                    "expiration_date": datetime.date(2022, 2, 10),
-                    "price": 600
-                },
-                {
-                    "name": "chicken",
-                    "expiration_date": datetime.date(2022, 2, 5),
-                    "price": 120
-                },
-                {
-                    "name": "duck",
-                    "expiration_date": datetime.date(2022, 2, 1),
-                    "price": 160
-                }
-            ],
-            ["duck"]
-        ),
+def test_outdated_products(mock_time: mock.Mock) -> None:
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
     ]
-)
-def test_outdated_products(mock_today: mock.Mock,
-                           products: list[dict],
-                           result: list[str]) -> None:
-    mock_today.return_value = datetime.date(2022, 2, 5)
-    assert outdated_products(products) == result
+    mock_time.date.today.return_value = datetime.date(2022, 2, 2)
+    assert outdated_products(products) == ["duck"]
