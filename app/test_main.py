e1a165bb8007bc73a1ee8363f05273f8dc61ae50
Jorjio <Jorjio22@gmail.com>
2023-06-12 20:19:34 +0000

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+import datetime
+
+from app.main import outdated_products
+
+
+def test_outdated_products_corrected_result() -> None:
+    assert outdated_products(
+        [
+            {
+                "name": "salmon",
+                "expiration_date": datetime.date(2023, 6, 11),
+                "price": 600
+            },
+            {
+                "name": "chicken",
+                "expiration_date": datetime.date.today(),
+                "price": 120
+            }
+        ]
+    ) == ["salmon"]
