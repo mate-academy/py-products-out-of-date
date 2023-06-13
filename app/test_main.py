c00bca2dcf1a3fdf0b55c3899b3e7682ab212660
Nikita <melniknikita2002@icloud.com>
2023-06-13 11:36:17 +0300

    remaded task

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
 from app.main import outdated_products
 from unittest import mock
+import datetime
 import pytest
 
 
@@ -7,7 +8,7 @@ class TestOutdatedProducts:
 
     @pytest.fixture
     def mocked_datetime(self) -> None:
-        with mock.patch("datetime.date") as mocked_test_datetime:
+        with mock.patch("app.main.datetime") as mocked_test_datetime:
             yield mocked_test_datetime
 
     def test_expect_list_of_outdated_products(
@@ -15,22 +16,22 @@ class TestOutdatedProducts:
             mocked_datetime: mock.MagicMock
     ) -> None:
 
-        mocked_datetime.today.return_value = "2022-02-08"
+        mocked_datetime.date.today.return_value = datetime.date(2022, 2, 6)
 
         result = outdated_products([
             {
                 "name": "salmon",
-                "expiration_date": "2022-02-10",
+                "expiration_date": datetime.date(2022, 2, 10),
                 "price": 600
             },
             {
                 "name": "chicken",
-                "expiration_date": "2022-02-08",
+                "expiration_date": datetime.date(2022, 2, 6),
                 "price": 120
             },
             {
                 "name": "duck",
-                "expiration_date": "2022-02-01",
+                "expiration_date": datetime.date(2022, 2, 1),
                 "price": 160
             }
         ])
