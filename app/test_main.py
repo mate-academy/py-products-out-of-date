a94be5bdb0ac36587e98945f5423c405c78379be
Denys Seliverstov <catsperr322@gmail.com>
2023-06-17 14:27:24 +0300

    Fixed

--- app/test_main.py
+++ app/test_main.py
@@ -18,28 +18,28 @@ def test_mock_datetime() -> Any:
 @pytest.fixture
 def test_fixture() -> list:
     products = [
-    {
-     "name": "salmon",
-     "expiration_date": datetime.date(2023, 6, 25),
-     "price": 600
-    },
-    {
-     "name": "chicken",
-     "expiration_date": datetime.date(2023, 6, 17),
-     "price": 120
-    },
-    {
-     "name": "duck",
-     "expiration_date": datetime.date(2023, 6, 16),
-     "price": 160
-    }
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2023, 6, 25),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2023, 6, 17),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2023, 6, 16),
+            "price": 160
+        }
     ]
     return products
 
 
 def test_outdated_products_is_duck(
- test_fixture: list,
- test_mock_datetime: Any
+        test_fixture: list,
+        test_mock_datetime: Any
 ) -> None:
     test_mock_datetime.date.today.return_value = datetime.date(2023, 6, 17)
     assert outdated_products(test_fixture) == ["duck"]
