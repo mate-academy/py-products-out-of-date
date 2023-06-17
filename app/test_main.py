d6dc94a2fbea29210f6689e6faa6e51c4d2cbce6
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-17 23:26:58 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 from datetime import date
+from typing import Any
 import pytest
 from unittest import mock
 from app.main import outdated_products
@@ -7,43 +8,45 @@ from app.main import outdated_products
 @pytest.fixture()
 def products_list() -> list[dict]:
     return [
-                {
-                    "name": "salmon",
-                    "expiration_date": date(2022, 2, 10),
-                    "price": 600
-                },
-                {
-                    "name": "chicken",
-                    "expiration_date": date(2022, 2, 5),
-                    "price": 120
-                },
-                {
-                    "name": "duck",
-                    "expiration_date": date(2022, 2, 1),
-                    "price": 160
-                }
-        ]
+        {
+            "name": "salmon",
+            "expiration_date": date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": date(2022, 2, 1),
+            "price": 160
+        }
+    ]
 
 
 @mock.patch("datetime.date")
-def test_all_products_are_dated(mocked_date, products_list) -> None:
+def test_all_products_are_dated(mocked_date: Any,
+                                products_list: list[dict]) -> None:
     mocked_date.today.return_value = date(2022, 2, 1)
     assert outdated_products(products_list) == []
 
 
 @mock.patch("datetime.date")
-def test_outdated_duck_chicken(mocked_date, products_list) -> None:
+def test_outdated_duck(mocked_date: Any, products_list: list[dict]) -> None:
     mocked_date.today.return_value = date(2022, 2, 3)
     assert outdated_products(products_list) == ["duck"]
 
 
 @mock.patch("datetime.date")
-def test_outdated_duck(mocked_date, products_list) -> None:
+def test_outdated_duck_chicken(mocked_date: Any,
+                               products_list: list[dict]) -> None:
     mocked_date.today.return_value = date(2022, 2, 6)
     assert outdated_products(products_list) == ["chicken", "duck"]
 
 
 @mock.patch("datetime.date")
-def test_outdated_all(mocked_date, products_list) -> None:
+def test_outdated_all(mocked_date: Any, products_list: list[dict]) -> None:
     mocked_date.today.return_value = date(2022, 2, 20)
     assert outdated_products(products_list) == ["salmon", "chicken", "duck"]
