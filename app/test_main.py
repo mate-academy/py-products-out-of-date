1349663221373df40b2b37a43f05e842862fd1ea
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-06-25 21:39:30 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,45 @@
 import datetime
-from unittest import mock
+
+import pytest
 
 from app.main import outdated_products
 
+from unittest import mock
 
-@mock.patch("app.main.datetime.time")
-def test_outdated_products(mocked_date: datetime) -> None:
-    mocked_date.date.today.return_value = datetime.date(2022, 2, 2)
+from typing import Any
 
+
+@pytest.fixture
+def test_mock_datetime() -> Any:
+    with mock.patch("app.main.datetime") as mocked_datetime:
+        yield mocked_datetime
+
+
+@pytest.fixture
+def test_fixture() -> list:
     products = [
-        {"name": "salmon",
-         "expiration_date": datetime.date(2022, 2, 10),
-         "price": 600},
-        {"name": "chicken",
-         "expiration_date": datetime.date(2022, 2, 5),
-         "price": 120},
-        {"name": "duck",
-         "expiration_date": datetime.date(2022, 2, 1),
-         "price": 160},
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2023, 6, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2023, 6, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2023, 5, 1),
+            "price": 160
+        }
     ]
+    return products
 
-    assert outdated_products(products) == ["salmon", "chicken", "duck"]
+
+def test_outdated_products_is_duck(
+        test_fixture: list,
+        test_mock_datetime: Any
+) -> None:
+    test_mock_datetime.date.today.return_value = datetime.date(2023, 6, 2)
+    assert outdated_products(test_fixture) == ["duck"]
