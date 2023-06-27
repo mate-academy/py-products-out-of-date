773bf74307bd42035c5a7b7a935dd3b161475190
laskarj <koks.adekvat@gmail.com>
2023-06-27 22:57:42 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+import datetime
+import pytest
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "data, result, message", [
+        ([{"name": "salmon",
+           "expiration_date": datetime.date(2023, 6, 27),
+           "price": 600
+           }],
+         [],
+         "If date is today, product is not out."),
+        ([{"name": "pork",
+           "expiration_date": datetime.date(2023, 6, 26),
+           "price": 600}],
+         ["pork"],
+         "If date is today, product is not out.")]
+)
+def test_outdated_products(
+        data: list,
+        result: list,
+        message: str,
+) -> None:
+
+    assert outdated_products(data) == result, message
