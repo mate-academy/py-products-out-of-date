1f8c62ba26311a49d43fcef14630effec52f8de3
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-07-28 14:37:24 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -15,29 +15,32 @@ def mock_today_date() -> date:
 @pytest.mark.parametrize(
     "products, expected_outdated",
     [
-        [
-            {
-                "name": "salmon",
-                "expiration_date": date(2022, 2, 10),
-                "price": 600,
-            },
-            {
-                "name": "chicken",
-                "expiration_date": date(2022, 2, 5),
-                "price": 120,
-            },
-            {
-                "name": "duck",
-                "expiration_date": date(2022, 2, 1),
-                "price": 160,
-            },
-        ],
-        ["duck"],
-    ],
+        (
+                [
+                    {
+                        "name": "salmon",
+                        "expiration_date": date(2022, 2, 10),
+                        "price": 600
+                    },
+                    {
+                        "name": "chicken",
+                        "expiration_date": date(2022, 2, 5),
+                        "price": 120
+                    },
+                    {
+                        "name": "duck",
+                        "expiration_date": date(2022, 2, 1),
+                        "price": 160
+                    }
+                ],
+                ["duck"]
+        ),
+
+    ]
 )
 def test_outdated_products(
         mock_today_date: date,
-        products: list[dict],
+        products: list,
         expected_outdated: list) -> None:
     with mock.patch("app.main.date") as mock_date:
         mock_date.today.return_value = mock_today_date
