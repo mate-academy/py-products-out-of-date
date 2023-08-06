05bc31b5a32f56e3ed9f1e293352d6a3a14521f2
Marina <marishka28023@gmail.com>
2023-08-06 20:14:47 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,33 @@
-# write your code here
+from unittest import mock
+
+import pytest
+import datetime
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "products",
+    [
+        (
+                [
+                    {"name": "salmon",
+                     "expiration_date": datetime.date(2022, 2, 10)
+                     },
+                    {"name": "chicken",
+                     "expiration_date": datetime.date(2022, 2, 2)
+                     },
+                    {"name": "duck",
+                     "expiration_date": datetime.date(2022, 2, 1)
+                     }
+                ]
+        )
+    ]
+)
+@mock.patch("app.main.datetime")
+def test_outdated_products(
+        mocked_date,
+        products: list
+) -> None:
+    mocked_date.date.today.return_value = datetime.date(2022, 2, 2)
+    assert outdated_products(products) == ["duck"]
