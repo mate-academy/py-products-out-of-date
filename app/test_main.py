f5f83a39e12c39d3b480c20f4781f8c33fa69f9b
Dima Khrystenko <hrystanko.dmytro@outlook.com>
2023-08-14 10:04:17 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+from app.main import outdated_products
+import datetime
+import pytest
+
+
+@pytest.fixture()
+def first_product_template() -> dict:
+    return {
+        "name": "chicken",
+        "expiration_date": datetime.date(2023, 8, 13),
+        "price": 120,
+    }
+
+
+@pytest.fixture()
+def second_product_template() -> dict:
+    return {
+        "name": "duck",
+        "expiration_date": datetime.date(2023, 8, 14),
+        "price": 160,
+    }
+
+
+@pytest.fixture()
+def list_to_test(
+    first_product_template: dict, second_product_template: dict
+) -> list:
+    return [
+        first_product_template,
+        second_product_template,
+    ]
+
+
+def test_correct_function_result(list_to_test: list) -> None:
+    assert outdated_products(list_to_test) == ["chicken"]
