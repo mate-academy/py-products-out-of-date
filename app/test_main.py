82712c125d53b2d05ce3be847af75eb08209b107
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-08-23 11:33:56 +0300

    add data

--- app/test_main.py
+++ app/test_main.py
@@ -20,6 +20,10 @@ def test_outdated_products(mocked_date: mock.MagicMock) -> None:
         {"name": "duck",
          "expiration_date": datetime.date(2022, 2, 1),
          "price": 160
+         },
+        {"name": "bread",
+         "expiration_date": datetime.date(2022, 2, 2),
+         "price": 160
          }
     ]
 
