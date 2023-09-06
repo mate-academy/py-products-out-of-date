ec86f78d8a82946f1d6f5dbb5dc4b2a159563cfc
struhanets <posseydon87@gmail.com>
2023-09-06 12:54:06 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,52 @@
-# write your code here
+from datetime import date
+from unittest import mock
+
+from app.main import outdated_products
+
+
+def test_function_datetime_when_all_products_are_fresh() -> None:
+    with mock.patch("datetime.date") as mocked_date:
+        mocked_date.today.return_value = date(2022, 2, 1)
+        result = outdated_products([
+            {
+                "name": "salmon",
+                "expiration_date": date(2022, 2, 10),
+                "price": 600
+            },
+            {
+                "name": "chicken",
+                "expiration_date": date(2022, 2, 5),
+                "price": 120
+            },
+            {
+                "name": "duck",
+                "expiration_date": date(2022, 2, 1),
+                "price": 160
+            }
+        ])
+
+        assert result == []
+
+
+def test_function_datetime_all_products_are_outdated() -> None:
+    with mock.patch("datetime.date") as mocked_date:
+        mocked_date.today.return_value = date(2022, 2, 11)
+        result = outdated_products([
+            {
+                "name": "salmon",
+                "expiration_date": date(2022, 2, 10),
+                "price": 600
+            },
+            {
+                "name": "chicken",
+                "expiration_date": date(2022, 2, 5),
+                "price": 120
+            },
+            {
+                "name": "duck",
+                "expiration_date": date(2022, 2, 1),
+                "price": 160
+            }
+        ])
+
+        assert result == ["salmon", "chicken", "duck"]
