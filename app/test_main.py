9941f139b56993a0ae4c82eb999a3d45e94812da
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-09-13 22:47:29 +0100

    out of date

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,40 @@
-# write your code here
+from unittest import mock
+import pytest
+from app.main import outdated_products, datetime
+from typing import Callable
+
+
+@pytest.fixture
+def mocked_date() -> mock:
+    with mock.patch("datetime.date") as mocked_date:
+        yield mocked_date
+
+
+@pytest.fixture()
+def datetime_now() -> datetime:
+    return datetime.date(2022, 2, 2)
+
+
+@pytest.fixture
+def food() -> list:
+    return \
+        [
+            {
+                "name": "chicken",
+                "expiration_date": datetime.date(2022, 2, 2),
+                "price": 120
+            },
+            {
+                "name": "duck",
+                "expiration_date": datetime.date(2022, 2, 1),
+                "price": 160
+            }
+        ]
+
+
+def test_expiration_day_today_not_outdated(food: list,
+                                           datetime_now: Callable,
+                                           mocked_date: Callable)\
+        -> AssertionError:
+    mocked_date.today.return_value = datetime_now
+    assert outdated_products(food) == ["duck"]
