0c36a4291319b90e6c1d3ecfd147266e988797bc
Artem <hartem472@gmail.com>
2023-09-20 22:34:01 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,44 +6,44 @@ from app.main import outdated_products
 
 @pytest.mark.parametrize("test_input, expected", [
     (
-            [
-                {
-                    "name": "лосось",
-                    "expiration_date": datetime.date(2022, 2, 1),
-                    "price": 600
-                }
-            ],
-            ['лосось']
+        [
+            {
+                "name": "losos",
+                "expiration_date": datetime.date(2022, 2, 1),
+                "price": 600
+            }
+        ],
+        ["losos"]
     ),
 
     (
-            [
-                {
-                    "name": "утка",
-                    "expiration_date": datetime.date(2022, 2, 3),
-                    "price": 160
-                }
-            ],
-            []
+        [
+            {
+                "name": "duck",
+                "expiration_date": datetime.date(2022, 2, 3),
+                "price": 160
+            }
+        ],
+        []
     ),
     (
-            [],
-            []
+        [],
+        []
     ),
     (
-            [
-                {
-                    "name": "говядина",
-                    "expiration_date": datetime.date(2022, 2, 2,),
-                    "price": 250
-                }
-            ],
-            []
+        [
+            {
+                "name": "meat",
+                "expiration_date": datetime.date(2022, 2, 2,),
+                "price": 250
+            }
+        ],
+        []
     )
 ])
-def test_outdated_products(test_input, expected):
+def test_outdated_products(test_input: list, expected: list) -> None:
     mock_today_date = datetime.date(2022, 2, 2)
 
-    with patch('app.main.datetime.date') as mock_date:
+    with patch("app.main.datetime.date") as mock_date:
         mock_date.today.return_value = mock_today_date
         assert outdated_products(test_input) == expected
