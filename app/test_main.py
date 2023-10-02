959abb851a01aafc8728e740aac5f32f8b6c314d
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-10-02 19:28:16 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,51 +1,49 @@
 import unittest
 from app.main import outdated_products
-from datetime import date
-from unittest.mock import patch
+import datetime
 
 
 class TestOutdatedProducts(unittest.TestCase):
 
+    def test_empty_product_list(self) -> None:
+        products = []
+        result = outdated_products(products)
+        self.assertEqual(result, [])
+
     def test_no_outdated_products(self) -> None:
+        today = datetime.date.today()
         products = [
-            {
-                "name": "salmon",
-                "expiration_date": date(2022, 2, 15),
-                "price": 600
-            },
-            {
-                "name": "chicken",
-                "expiration_date": date(2022, 3, 5),
-                "price": 120
-            }
+            {"name": "Product1", "expiration_date":
+                today + datetime.timedelta(days=5)},
+            {"name": "Product2", "expiration_date":
+                today + datetime.timedelta(days=10)}
         ]
-        with patch("datetime.date") as mock_date:
-            mock_date.today.return_value = date(2022, 2, 2)
-            result = outdated_products(products)
-            self.assertEqual(result, [])
+        result = outdated_products(products)
+        self.assertEqual(result, [])
 
     def test_some_outdated_products(self) -> None:
+        today = datetime.date.today()
         products = [
-            {
-                "name": "salmon",
-                "expiration_date": date(2022, 2, 10),
-                "price": 600
-            },
-            {
-                "name": "chicken",
-                "expiration_date": date(2022, 2, 5),
-                "price": 120
-            },
-            {
-                "name": "duck",
-                "expiration_date": date(2022, 2, 1),
-                "price": 160
-            }
+            {"name": "Product1", "expiration_date":
+                today - datetime.timedelta(days=5)},
+            {"name": "Product2", "expiration_date":
+                today + datetime.timedelta(days=5)},
+            {"name": "Product3", "expiration_date":
+                today + datetime.timedelta(days=10)}
         ]
-        with patch("datetime.date") as mock_date:
-            mock_date.today.return_value = date(2022, 2, 2)
-            result = outdated_products(products)
-            self.assertEqual(result, ["duck"])
+        result = outdated_products(products)
+        self.assertEqual(result, ["Product1"])
+
+    def test_all_outdated_products(self) -> None:
+        today = datetime.date.today()
+        products = [
+            {"name": "Product1", "expiration_date":
+                today - datetime.timedelta(days=5)},
+            {"name": "Product2", "expiration_date":
+                today - datetime.timedelta(days=1)}
+        ]
+        result = outdated_products(products)
+        self.assertEqual(result, ["Product1", "Product2"])
 
 
 if __name__ == "__main__":
