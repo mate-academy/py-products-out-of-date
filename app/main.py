3b339e1c400266aa764a89bf5846133f22712b8d
Alex Ilchenko <alec.ilchenko@gmail.com>
2023-10-24 05:37:36 +0400

    Solution

--- app/main.py
+++ app/main.py
@@ -4,3 +4,23 @@ import datetime
 def outdated_products(products: list) -> list:
     return [product["name"] for product in products
             if product["expiration_date"] < datetime.date.today()]
+
+products = [
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2023, 10, 21),
+            "price": 200
+        },
+        {
+            "name": "fish",
+            "expiration_date": datetime.date(2023, 10, 21),
+            "price": 200
+        },
+        {
+            "name": "rabbit",
+            "expiration_date": datetime.date(2023, 10, 21),
+            "price": 200
+        }
+    ]
+print(datetime.date.today())
+print(outdated_products(products))
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,52 @@
-# write your code here
+import datetime
+import pytest
+from unittest.mock import patch
+from app.main import outdated_products
+
+
+
+@pytest.fixture()
+def products_template():
+    return [
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2023, 10, 29),
+            "price": 200
+        },
+        {
+            "name": "fish",
+            "expiration_date": datetime.date(2023, 10, 29),
+            "price": 200
+        },
+        {
+            "name": "rabbit",
+            "expiration_date": datetime.date(2023, 10, 29),
+            "price": 200
+        }
+    ]
+
+
+def test_all_product_fresh(products_template):
+    today = datetime.date(2023, 10, 28)
+    with patch("datetime.date") as mock_date:
+        mock_date.today.return_value = today
+        result = outdated_products(products_template)
+        assert result == []
+
+
+def test_expiration_day_today_outdated(products_template):
+    today = datetime.date(2023, 10, 29)
+    with patch("datetime.date") as mock_date:
+        mock_date.today.return_value = today
+        result = outdated_products(products_template)
+        assert result == [], (
+            "Product with expiration date equals today is not outdated."
+        )
+
+
+def test_all_expired(products_template):
+    today = datetime.date(2023, 10, 30)
+    with patch("datetime.date") as mock_date:
+        mock_date.today.return_value = today
+        result = outdated_products(products_template)
+        assert result == ["duck", "fish", "rabbit"]
