c48a11ab8b26857c091b547be911d53058f8d413
Vladyslav Sabo <lebwa1337@gmail.com>
2023-10-31 20:08:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,3 @@ import datetime
 def outdated_products(products: list) -> list:
     return [product["name"] for product in products
             if product["expiration_date"] < datetime.date.today()]
-
-# @mock.patch("app.main.datetime")
-# def test_check_if_all_expired(mocked_time, products_template) -> None:
-#     mocked_time.date.today.return_value = datetime.date(2023, 2, 3)
-#     assert outdated_products(products_template) == ["salmon", "chicken", "duck"]
--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ from app.main import outdated_products
 
 
 @pytest.fixture()
-def products_template() -> list:
+def products_temp() -> list:
     return [
         {
             "name": "salmon",
@@ -26,6 +26,19 @@ def products_template() -> list:
         }]
 
 
-@mock.patch("app.main.datetime.date.today", return_value=datetime.date(2023, 2, 3))
-def test_check_if_all_expired(mocked_time, products_template) -> None:
-    assert outdated_products(products_template) == ["salmon", "chicken", "duck"]
+@mock.patch("app.main.datetime")
+def test_check_if_all_expired(
+        mocked_time: datetime,
+        products_temp: list
+) -> None:
+    mocked_time.date.today.return_value = datetime.date(2023, 2, 3)
+    assert outdated_products(products_temp) == ["salmon", "chicken", "duck"]
+
+
+@mock.patch("app.main.datetime")
+def test_check_if_expiration_date_equals_today(
+        mocked_time: datetime,
+        products_temp: list
+) -> None:
+    mocked_time.date.today.return_value = datetime.date(2022, 2, 2)
+    assert outdated_products(products_temp) == ["duck"]
