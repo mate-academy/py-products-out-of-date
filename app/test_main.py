62e88656b46602a01c7b7fe8dca93cfe18cd5bc3
Ruslan <rusipbox@gmail.com>
2023-11-03 23:09:25 +0200

    Test3

--- app/test_main.py
+++ app/test_main.py
@@ -1,60 +1,34 @@
 import datetime
-from unittest.mock import patch
-import pytest
 from app.main import outdated_products
-from typing import Any
 
 
-@pytest.fixture
-def mock_today() -> Any:
-    with patch("app.test_main.datetime") as mock_datetime:
-        mock_datetime.date.today.return_value = datetime.date(2022, 2, 2)
-        yield mock_datetime.date.today
-
-
-def test_outdated_products(mock_today: Any) -> None:
-    products = [
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 2, 1),
-            "price": 160
-        }
-    ]
-
-    result = outdated_products(products)
-    assert result == ["duck"]
-
-    products = [
-        {
-            "name": "apple",
-            "expiration_date": datetime.date(2022, 2, 3),
-            "price": 1
-        },
-        {
-            "name": "banana",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 2
-        }
-    ]
-
-    result = outdated_products(products)
-    assert result == []
-
+def test_empty_products_list():
     products = []
-
-    result = outdated_products(products)
-    assert result == []
+    assert outdated_products(products) == []
 
 
-if __name__ == "__main__":
-    pytest.main()
+def test_no_outdated_products():
+    products = [
+        {"name": "Product1", "expiration_date": datetime.date(2023, 12, 31)},
+        {"name": "Product2", "expiration_date": datetime.date(2023, 10, 15)}
+    ]
+    assert outdated_products(products) == []
+
+
+def test_some_outdated_products():
+    products = [
+        {"name": "Product1", "expiration_date": datetime.date(2022, 1, 1)},
+        {"name": "Product2", "expiration_date": datetime.date(2023, 3, 15)},
+        {"name": "Product3", "expiration_date": datetime.date(2022, 2, 1)}
+    ]
+    expected_result = ["Product1", "Product2", "Product3"]
+    assert outdated_products(products) == expected_result
+
+
+def test_all_outdated_products():
+    products = [
+        {"name": "Product1", "expiration_date": datetime.date(2021, 1, 1)},
+        {"name": "Product2", "expiration_date": datetime.date(2020, 3, 15)}
+    ]
+    expected_result = ["Product1", "Product2"]
+    assert outdated_products(products) == expected_result
