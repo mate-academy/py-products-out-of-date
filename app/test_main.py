1384649d5024366e0075ac52e09f0e3e989feb3f
Ruslan <rusipbox@gmail.com>
2023-11-03 23:11:04 +0200

    Test4

--- app/test_main.py
+++ app/test_main.py
@@ -2,12 +2,12 @@ import datetime
 from app.main import outdated_products
 
 
-def test_empty_products_list():
+def test_empty_products_list() -> None:
     products = []
     assert outdated_products(products) == []
 
 
-def test_no_outdated_products():
+def test_no_outdated_products() -> None:
     products = [
         {"name": "Product1", "expiration_date": datetime.date(2023, 12, 31)},
         {"name": "Product2", "expiration_date": datetime.date(2023, 10, 15)}
@@ -15,7 +15,7 @@ def test_no_outdated_products():
     assert outdated_products(products) == []
 
 
-def test_some_outdated_products():
+def test_some_outdated_products() -> None:
     products = [
         {"name": "Product1", "expiration_date": datetime.date(2022, 1, 1)},
         {"name": "Product2", "expiration_date": datetime.date(2023, 3, 15)},
@@ -25,7 +25,7 @@ def test_some_outdated_products():
     assert outdated_products(products) == expected_result
 
 
-def test_all_outdated_products():
+def test_all_outdated_products() -> None:
     products = [
         {"name": "Product1", "expiration_date": datetime.date(2021, 1, 1)},
         {"name": "Product2", "expiration_date": datetime.date(2020, 3, 15)}
