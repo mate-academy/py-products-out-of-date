8f81df4efa3ac0a78aba01c0b373945e96146b25
Denysov Viacheslav <wdenisov113@gmail.com>
2023-12-11 12:07:28 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,54 @@
-# write your code here
+import pytest
+import datetime
+from app.main import outdated_products
+from freezegun import freeze_time
+
+
+@pytest.mark.parametrize("products, expected_result", [
+    ([{
+        "name": "salmon",
+        "expiration_date": datetime.date(2021, 2, 10),
+        "price": 600
+    }], ["salmon"]),
+    ([{
+        "name": "chicken",
+        "expiration_date": datetime.date(2023, 1, 1),
+        "price": 120
+    }], []),
+    ([{
+        "name": "duck",
+        "expiration_date": datetime.date(2022, 3, 2),
+        "price": 160
+    }], []),
+    ([{
+        "name": "apple",
+        "expiration_date": datetime.date(2022, 1, 2),
+        "price": 160
+    }], ["apple"]),
+    ([{
+        "name": "potato",
+        "expiration_date": datetime.date(2022, 3, 2),
+        "price": 160
+    }], []),
+    ([{
+        "name": "strawberry",
+        "expiration_date": datetime.date(2022, 2, 2),
+        "price": 160
+    }], []),
+    ([{
+        "name": "pineapple",
+        "expiration_date": datetime.date(2022, 2, 1),
+        "price": 160
+    }], ["pineapple"]),
+    ([{
+        "name": "plum",
+        "expiration_date": datetime.date(2022, 2, 3),
+        "price": 160
+    }], [])
+])
+@freeze_time("2022-02-02")
+def test_outdated_products(
+        products: list,
+        expected_result: list | None
+) -> None:
+    assert outdated_products(products) == expected_result
