18ce38495578963560b814195d5d62ffa8f0333c
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2023-12-26 22:40:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import datetime
 
 
+def get_date() -> datetime.date:
+    return datetime.date.today()
+
+
 def outdated_products(products: list) -> list:
     return [product["name"] for product in products
-            if product["expiration_date"] < datetime.date.today()]
+            if product["expiration_date"] < get_date()]
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+from app.main import outdated_products
+import datetime
+
+
+class TestOutdatedProducts(TestCase):
+
+    @patch("app.main.get_date")
+    def test_outdated_products_no_outdated_products(self,
+                                                    mock_date: MagicMock
+                                                    ) -> None:
+        mock_date.return_value = datetime.date(2022, 2, 2)
+        products = [
+            {"name": "salmon", "expiration_date": datetime.date(
+                2022, 2, 10), "price": 600},
+            {"name": "chicken", "expiration_date": datetime.date(
+                2022, 2, 5), "price": 120},
+        ]
+        self.assertEqual(outdated_products(products), [])
+
+    @patch("app.main.get_date")
+    def test_outdated_products_some_outdated_products(self,
+                                                      mock_date: MagicMock
+                                                      ) -> None:
+        mock_date.return_value = datetime.date(2022, 2, 2)
+        products = [
+            {"name": "salmon", "expiration_date": datetime.date(
+                2022, 2, 10), "price": 600},
+            {"name": "chicken", "expiration_date": datetime.date(
+                2022, 2, 5), "price": 120},
+            {"name": "duck", "expiration_date": datetime.date(
+                2022, 1, 30), "price": 160},
+        ]
+        self.assertEqual(outdated_products(products), ["duck"])
