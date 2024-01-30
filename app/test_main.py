6f33d3a96d9ea253954b97dbeb1aaff5d1ca7e47
PythonZem <pythonzem@gmail.com>
2024-01-30 16:41:34 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -4,33 +4,78 @@ from unittest import mock
 import pytest
 
 
-@pytest.mark.parametrize (
-    "products,result",
+@pytest.mark.parametrize(
+    "today,products,result",
     [
-        ([
-            {
-                "name": "salmon",
-                "expiration_date": datetime.date(2022, 2, 10),
-                "price": 600
-            },
-            {
-                "name": "chicken",
-                "expiration_date": datetime.date(2022, 2, 5),
-                "price": 120
-            },
-            {
-                "name": "duck",
-                "expiration_date": datetime.date(2022, 2, 1),
-                "price": 160
-            }
-        ],
-        ["duck"]),
+        (datetime.date(2022, 2, 2),
+         [
+             {
+                 "name": "salmon",
+                 "expiration_date": datetime.date(2022, 2, 10),
+                 "price": 600
+             },
+             {
+                 "name": "chicken",
+                 "expiration_date": datetime.date(2022, 2, 5),
+                 "price": 120
+             },
+             {
+                 "name": "duck",
+                 "expiration_date": datetime.date(2022, 2, 1),
+                 "price": 160
+             }],
+         ["duck"]
+         ),
+        (datetime.date(2022, 2, 14),
+         [
+             {
+                 "name": "salmon",
+                 "expiration_date": datetime.date(2022, 2, 10),
+                 "price": 600
+             },
+             {
+                 "name": "chicken",
+                 "expiration_date": datetime.date(2022, 2, 5),
+                 "price": 120
+             },
+             {
+                 "name": "duck",
+                 "expiration_date": datetime.date(2022, 2, 15),
+                 "price": 160
+             }],
+         ["salmon", "chicken"]
+         ),
+        (datetime.date(2022, 2, 2),
+         [
+             {
+                 "name": "salmon",
+                 "expiration_date": datetime.date(2022, 2, 10),
+                 "price": 600
+             },
+             {
+                 "name": "chicken",
+                 "expiration_date": datetime.date(2022, 2, 2),
+                 "price": 120
+             },
+             {
+                 "name": "duck",
+                 "expiration_date": datetime.date(2022, 2, 15),
+                 "price": 160
+             }],
+         []
+         )
     ],
     ids=[
-        "For 'duck' expiration date out the date "
+        "For 'duck' expiration date out the date",
+        "For 'salmon', and 'chicken' expiration date out the date",
+        "All products are fresh",
     ]
 )
-def test_outdated_products(products: list, result: list) -> None:
-    with mock.patch("datetime.date.today()") as mock_date:
-        mock_date.return_value = datetime.date(2022, 2, 2)
-        assert outdated_products(products= products) == result
+def test_outdated_products(
+        today: datetime,
+        result: list,
+        products: list[dict],
+) -> None:
+    with mock.patch("datetime.date") as mocked_date:
+        mocked_date.today.return_value = today
+        assert outdated_products(products) == result
