7ad3e968a44e8775ddee64b16c29952c0865fca6
AlexHCom <alexhcomgrit@gmail.com>
2024-01-30 17:41:45 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,75 +5,14 @@ from app.main import outdated_products
 from typing import List, Dict
 
 
-@patch("app.main.datetime")
-def test_outdated_products_today(mock_datetime: any) -> None:
-    mock_datetime.date.today.return_value = datetime.date(2022, 2, 2)
-
-    products: List[Dict[str, any]] = [
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 2, 1),
-            "price": 160
-        }
-    ]
-
-    result: List[str] = outdated_products(products)
-    assert result == ["duck"]
-
-
-@patch("app.main.datetime")
-def test_outdated_products_future(mock_datetime: any) -> None:
-    mock_datetime.date.today.return_value = datetime.date(2022, 1, 1)
-
-    products: List[Dict[str, any]] = [
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 2, 1),
-            "price": 160
-        }
-    ]
-
-    result: List[str] = outdated_products(products)
-    assert result == []
-
-
-@patch("app.main.datetime")
-def test_outdated_products_empty_list(mock_datetime: any) -> None:
-    mock_datetime.date.today.return_value = datetime.date(2022, 2, 2)
-
-    products: List[Dict[str, any]] = []
-
-    result: List[str] = outdated_products(products)
-    assert result == []
-
-
-@patch("app.main.datetime")
+@patch('app.main.datetime')
 def test_outdated_products_no_expiration_date(mock_datetime: any) -> None:
     mock_datetime.date.today.return_value = datetime.date(2022, 2, 2)
 
     products: List[Dict[str, any]] = [
         {
             "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
             "price": 600
         },
         {
@@ -83,10 +22,9 @@ def test_outdated_products_no_expiration_date(mock_datetime: any) -> None:
         },
         {
             "name": "duck",
-            "expiration_date": datetime.date(2022, 2, 1),
             "price": 160
         }
     ]
 
     result: List[str] = outdated_products(products)
-    assert result == ["duck"]
+    assert result == ['duck']
