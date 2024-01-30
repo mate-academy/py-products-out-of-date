c29b65eef676bea6baa9f32f4d7fb98161fd8b0f
barabarinov <barabarinov@gmail.com>
2024-01-30 19:22:26 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,59 @@
-# write your code here
+import pytest
+import datetime
+
+from unittest.mock import patch
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "today_date,expected_names",
+    [
+        (
+            datetime.date(2024, 1, 28),
+            ["duck"],
+        ),
+        (
+            datetime.date(2024, 2, 6),
+            ["chicken", "duck"],
+        ),
+        (
+            datetime.date(2024, 2, 11),
+            ["salmon", "chicken", "duck"],
+        ),
+        (
+            datetime.date(2024, 1, 1),
+            [],
+        ),
+    ],
+    ids=[
+        "Today is 2024-01-28, one product outdated (duck)",
+        "Today is 2024-02-06, two products outdated (chicken, duck)",
+        "Today is 2024-02-11, three products outdated (salmon, chicken, duck)",
+        "Today is 2024-01-01, no products outdated",
+    ]
+)
+def test_outdated_products(
+    today_date: datetime.date,
+    expected_names: list[str],
+) -> None:
+    with patch("datetime.date.today") as mock_date:
+        mock_date.return_value = today_date
+
+        products = [
+            {
+                "name": "salmon",
+                "expiration_date": datetime.date(2024, 2, 10),
+                "price": 600,
+            },
+            {
+                "name": "chicken",
+                "expiration_date": datetime.date(2024, 2, 5),
+                "price": 120},
+            {
+                "name": "duck",
+                "expiration_date": datetime.date(2024, 1, 22),
+                "price": 160,
+            },
+        ]
+        assert outdated_products(products) == expected_names
+        mock_date.assert_called_once()
