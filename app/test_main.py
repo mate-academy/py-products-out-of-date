186212adc50cea054ae41305536b14778b74d37e
AlexHCom <alexhcomgrit@gmail.com>
2024-01-30 17:51:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,27 @@
 # app/test_main.py
-from unittest.mock import patch
 import datetime
 from app.main import outdated_products
 from typing import List, Dict
+import pytest
+from unittest.mock import patch
 
 
-@patch("app.main.datetime")
-def test_outdated_products_no_expiration_date(mock_datetime: any) -> None:
-    mock_datetime.date.today.return_value = datetime.date(2022, 2, 2)
+@pytest.mark.parametrize("input_products, expected_result", [
+    (
+        [
+            {"name": "salmon", "expiration_date": datetime.date(2022, 2, 10), "price": 600}, # Noqa E501
+            {"name": "chicken", "expiration_date": datetime.date(2022, 2, 5), "price": 120}, # Noqa E501
+            {"name": "duck", "price": 160}
+        ],
+        ["duck"]
+    ),
+    # Add more test cases as needed
+])
+def test_outdated_products_no_expiration_date(input_products: List[Dict[str, any]], expected_result: List[str]) -> None: # Noqa E501
+    mock_datetime = datetime.date(2022, 2, 2)
 
-    products: List[Dict[str, any]] = [
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "price": 160
-        }
-    ]
+    with patch("app.main.datetime") as mock_datetime_obj:
+        mock_datetime_obj.date.today.return_value = mock_datetime
 
-    result: List[str] = outdated_products(products)
-    assert result == ["duck"]
+        result: List[str] = outdated_products(input_products)
+        assert result == expected_result
