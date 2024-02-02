054836555d9e26fa808da2a8a176e1db12589944
barabarinov <barabarinov@gmail.com>
2024-02-02 14:52:45 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -2,5 +2,7 @@ import datetime
 
 
 def outdated_products(products: list) -> list:
+    d = datetime.date.today()
+    print(">>>> ", d)
     return [product["name"] for product in products
             if product["expiration_date"] < datetime.date.today()]
--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,7 @@ from app.main import outdated_products
 
 
 @pytest.mark.parametrize(
-    "today_date,expected_names",
+    "expected_date,expected_names",
     [
         (
             datetime.date(2024, 1, 28),
@@ -33,27 +33,28 @@ from app.main import outdated_products
     ]
 )
 def test_outdated_products(
-    today_date: datetime.date,
+    expected_date: datetime.date,
     expected_names: list[str],
 ) -> None:
-    with patch("datetime.date") as mock_date:
-        mock_date.today.return_value = today_date
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2024, 2, 10),
+            "price": 600,
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2024, 2, 5),
+            "price": 120},
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2024, 1, 22),
+            "price": 160,
+        },
+    ]
 
-        products = [
-            {
-                "name": "salmon",
-                "expiration_date": datetime.date(2024, 2, 10),
-                "price": 600,
-            },
-            {
-                "name": "chicken",
-                "expiration_date": datetime.date(2024, 2, 5),
-                "price": 120},
-            {
-                "name": "duck",
-                "expiration_date": datetime.date(2024, 1, 22),
-                "price": 160,
-            },
-        ]
+    with patch("datetime.date") as mock_date:
+        mock_date.today.return_value = expected_date
+
         assert outdated_products(products) == expected_names
         mock_date.assert_called_once()
