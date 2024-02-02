6d689e317a16f30a681c23208a9f17b0d1f02bab
barabarinov <barabarinov@gmail.com>
2024-02-02 14:55:23 +0200

    fixed, cleaned print from main

--- app/main.py
+++ app/main.py
@@ -2,7 +2,5 @@ import datetime
 
 
 def outdated_products(products: list) -> list:
-    d = datetime.date.today()
-    print(">>>> ", d)
     return [product["name"] for product in products
             if product["expiration_date"] < datetime.date.today()]
