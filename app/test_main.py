b60b78541c7ca965e39120aa462aecdc443bf902
IvanGuculyak <ivan.guculyak@gmail.com>
2024-02-06 19:14:06 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -13,17 +13,17 @@ from app.main import outdated_products
             [
                 {
                     "name": "salmon",
-                    "expiration_date": datetime.date(2022, 2, 10),
+                    "expiration_date": datetime.date(2024, 2, 10),
                     "price": 600
                 },
                 {
                     "name": "chicken",
-                    "expiration_date": datetime.date(2022, 2, 5),
+                    "expiration_date": datetime.date(2024, 2, 6),
                     "price": 120
                 },
                 {
                     "name": "duck",
-                    "expiration_date": datetime.date(2022, 2, 1),
+                    "expiration_date": datetime.date(2024, 2, 1),
                     "price": 160
                 }
             ],
@@ -34,10 +34,10 @@ from app.main import outdated_products
 )
 @mock.patch("app.main.datetime.date")
 def test_outdated_products(
-        mock_datime_date_today: mock.MagicMock,
+        mock_datime_date: mock.MagicMock,
         products: list,
         today_date: datetime,
         names_product: list
 ) -> None:
-    mock_datime_date_today.today.return_value = today_date
+    mock_datime_date.today.return_value = today_date
     assert outdated_products(products) == names_product
