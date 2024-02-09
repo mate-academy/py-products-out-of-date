f35d767bf06f5f3203e0e31ad6e0543be87cf6e0
mkuchuman <mkuchuman1@gmail.com>
2024-02-09 02:09:11 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,29 +7,29 @@ from app.main import outdated_products
 @pytest.mark.parametrize(
     "products, result, today",
     [
-       ( [
-            {
-                "name": "salmon",
-                "expiration_date": datetime.date(2022, 2, 10),
-                "price": 600
-            },
-            {
-                "name": "chicken",
-                "expiration_date": datetime.date(2022, 2, 5),
-                "price": 120
-            },
-            {
-                "name": "duck",
-                "expiration_date": datetime.date(2022, 2, 1),
-                "price": 160
-            }
-        ],
-        ['duck'],
-        datetime.date(2022, 2, 2),
-)
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ],
+            ["duck"],
+            datetime.date(2022, 2, 2),
+        )
     ]
-                            )
-
+)
 def test_products(products: list, result: list, today: datetime) -> list:
     with mock.patch("app.main.datetime") as mocked_data:
         mocked_data.date.today.return_value = today
