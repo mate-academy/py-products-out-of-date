cb78e41da0ad500691c54bcdd6c5cda5ae418abd
RomanHlodann <roman.hlodann@gmail.com>
2024-02-19 19:01:25 +0200

    tests for func outdated products

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,45 @@
-# write your code here
+import pytest
+import datetime
+
+from unittest import mock
+
+from app.main import outdated_products
+
+
+@pytest.fixture
+def products() -> list:
+    return [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]
+
+
+@pytest.mark.parametrize(
+    "best_by,result",
+    [
+        (datetime.date(2022, 2, 1), []),
+        (datetime.date(2022, 2, 2), ["duck"]),
+        (datetime.date(2022, 2, 12), ["salmon", "chicken", "duck"]),
+    ]
+)
+def test_correct_return_outdated_products(
+        products: list,
+        best_by: datetime.date,
+        result: list
+) -> None:
+    with mock.patch("app.main.datetime") as mock_datetime:
+        mock_datetime.date.today.return_value = best_by
+        assert outdated_products(products) == result
