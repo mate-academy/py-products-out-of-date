178ed2facf7b64d384900b488a8dee9df0163635
Maksim <sma4no.7@gmail.com>
2024-03-07 09:55:45 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,26 +2,31 @@ import datetime
 from unittest.mock import patch
 from app.main import outdated_products
 
+template = [
+    {
+        "name": "salmon",
+        "expiration_date": datetime.date(2022, 2, 2),  # Expiring today
+        "price": 600
+    },
+    {
+        "name": "chicken",
+        "expiration_date": datetime.date(2022, 2, 1),  # Expired yesterday
+        "price": 120
+    },
+    {
+        "name": "duck",
+        "expiration_date": datetime.date(2022, 2, 3),  # Expiring tomorrow
+        "price": 160
+    },
+]
+
 
 def test_outdated_products() -> None:
     with patch("app.main.datetime") as mock_datetime:
-        mock_datetime.today.return_value = datetime.datetime(2022, 2, 2)
         mock_datetime.date.today.return_value = datetime.date(2022, 2, 2)
 
-    assert outdated_products([
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 2, 1),
-            "price": 160
-        }
-    ]) == ["salmon", "chicken", "duck"]
+        assert "chicken" in outdated_products(template)
+
+        assert "salmon" not in outdated_products(template)
+
+        assert "duck" not in outdated_products(template)
