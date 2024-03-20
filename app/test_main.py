79d6d4fc810b67ecddca03955f5002e2dc9da95c
Mykola Setrakov <nikset1@gmail.com>
2024-03-20 17:00:28 +0200

    used black

--- app/test_main.py
+++ app/test_main.py
@@ -7,25 +7,23 @@ from app.main import outdated_products
 class TestOutdatedProducts:
 
     @patch("app.main.datetime")
-    def test_expected_result(
-            self, mock_today: MagicMock
-    ) -> None:
+    def test_expected_result(self, mock_today: MagicMock) -> None:
         products = [
             {
                 "name": "salmon",
                 "expiration_date": datetime.date(2022, 2, 10),
-                "price": 600
+                "price": 600,
             },
             {
                 "name": "chicken",
                 "expiration_date": datetime.date(2022, 2, 5),
-                "price": 120
+                "price": 120,
             },
             {
                 "name": "duck",
                 "expiration_date": datetime.date(2022, 2, 1),
-                "price": 160
-            }
+                "price": 160,
+            },
         ]
         mock_today.date.today.return_value = datetime.date(2022, 2, 2)
         assert outdated_products(products) == ["duck"]
