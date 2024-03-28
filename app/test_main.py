1e0ff954ae3fdd2f792ea2bd63377fec8ee56551
Mariia Sydoruk <marisydoruk12@gmail.com>
2024-03-28 16:20:27 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,64 @@
-# write your code here
+import datetime
+from unittest.mock import patch
+from app.main import outdated_products
+
+
+@patch("app.main.datetime")
+def test_outdated_products(mock_datetime: str) -> None:
+    mock_datetime.date.today.return_value = datetime.date(2022, 2, 2)
+
+    assert outdated_products([
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 7),
+            "price": 160
+        }
+    ]) == []
+
+    assert outdated_products([
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]) == ["salmon", "duck"]
+
+    assert outdated_products([
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]) == ["salmon", "chicken", "duck"]
+
+    assert outdated_products([]) == []
